60eda526a978b4c4b8a918349e1003616066fdc8
Branden Stober <67252380+BrandenStoberReal@users.noreply.github.com>
2023-01-04 17:06:37 -0500

    Add project files.

---
+++ CMake-Test/CMake-Test.cpp
@@ -0,0 +1,12 @@
+﻿// CMake-Test.cpp : Defines the entry point for the application.
+//
+
+#include "CMake-Test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
